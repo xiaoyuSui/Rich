47ef3c289a4b7cf20a0bb0746dc73c0ec2ec99f1
AsteriaWqm <43130431+AsteriaWqm@users.noreply.github.com>
2019-09-06 13:57:53 +0800

    9.6 13：57

--- Rich/src/java/Entity/AllController.java
+++ Rich/src/java/Entity/AllController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.Query;
 
 /**
  *
@@ -30,28 +31,57 @@ public class AllController implements Serializable{
     
     //用于存放当前登录的用户数据
     private User current = null;
-    
-    private List<Project> projectList;
+ 
     private int kind=0;
-    
     
     public AllController(){
         current = new User("13671021552","wqm","password"); 
     }
     
-    private ProjectFacade getProjFacade() {
-        return projFacade;
+    //当前用户申请项目总数
+    public int MyProjectSum(){
+        int sum;
+        Query query = getGuardFacade().getEntityManager().createNamedQuery("Project.CountByUserTel").setParameter("userTel", current.getUserTel());
+        sum = query.
+        return sum;
     }
     
-    public List<Project> getProjectList(){
-        projectList = this.getProjFacade().findAll();
-        System.out.print("我在projectList里面！！！！！！！！！！！！！");
-        return projectList;
+    //当前用户守护项目总数
+    public int MyGuardSum(){
+        int sum;
+        return sum;
     }
     
-    public void setProjectList(){
-        
+    //当前用户资助项目总数
+    public int MyInvestProject(){
+        int sum;
+        return sum;
     }
+    
+    //当前用户守护的项目
+    public List<GuardProj> MyGurad(){
+        List<GuardProj> myGuard;
+        Query query = getGuardFacade().getEntityManager().createNamedQuery("GuardProj.findByUserTel").setParameter("userTel", current.getUserTel());
+        myGuard = query.getResultList();
+        return myGuard;
+    }
+    
+    //当前用户资助过的项目
+    public List<InvesteProj> MyInvest(){
+        List<InvesteProj> myInvest;
+        Query query = getInvestFacade().getEntityManager().createNamedQuery("InvesteProj.findByUserTel").setParameter("userTel", current.getUserTel());
+        myInvest =query.getResultList();
+        return myInvest;
+    }
+    
+    //当前用户申请过的项目
+    public List<Project> MyProject(){
+        List<Project> myProject;
+        Query query = getProjFacade().getEntityManager().createNamedQuery("Project.findByUserTel").setParameter("userTel", current.getUserTel());
+        myProject = query.getResultList();
+        return myProject;
+    }
+    
     
     public int getKind(){
         return kind;
@@ -60,5 +90,21 @@ public class AllController implements Serializable{
     public void setKind(int kind_){
         kind = kind_;
     }
-       
+    
+    private ProjectFacade getProjFacade() {
+        return projFacade;
+    }
+    
+    private InvesteProjFacade getInvestFacade() {
+        return investFacade;
+    }
+    
+    private UserFacade getUserFacade() {
+        return userFacade;
+    }
+    
+    private GuardProjFacade getGuardFacade() {
+        return guardFacade;
+    }
+        
 }
